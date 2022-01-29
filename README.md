3e618066b6115bd16aa899e31c877e0217258ca8
timonschapeler <54599452+timonschapeler@users.noreply.github.com>
2022-01-29 01:52:36 +0100

    Added functionality, second tab
    It is now possible to get the weekday for a date via a text field. Crashes for incorrect input format - needs fixing.

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ This project is able to generate random dates on a button press.
 It reveals the weekday on another button press.
 It can be used to train calculating the weekday for any random date.
 
+Added the option to check a date via a text field, date has to be given as 'yyyy-MM-dd'.
+
 ## Current date regime
 1700 - 2400
 
--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,12 @@ class _HomeState extends State<Home> {
       _isVisible = false;
     });
   }
+  final verifyDate = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(BuildContext context) => DefaultTabController(
+      length: 2,
+      child: Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
           title: const Text('Random Date Generator'),
@@ -41,69 +43,132 @@ class _HomeState extends State<Home> {
               ),
             ],
             ),
-        ],
+          ],
+        bottom: const TabBar(
+          tabs: [
+            Tab(text: 'Generate', icon: Icon(Icons.calendar_today)),
+            Tab(text: 'Verify', icon: Icon(Icons.check_rounded)),
+          ],
+        ),
       ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-          Text(
-              //'$currentDate'.substring(0,10) //display as yyyy-mm-dd
-              '$currentDate'.substring(8,10)+'.'+'$currentDate'.substring(5,7)+'.'+'$currentDate'.substring(0,4), //display as dd.mm.yyyy
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 35.0,
-              ),
-          ),
-          Visibility(
-          visible: _isVisible,
-            maintainSize: true,
-            maintainAnimation: true,
-            maintainState: true,
-            child: Text(
-              //'${currentDate.weekday}',
-              DateFormat('EEEE').format(currentDate),
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 35.0,
-              )
-          ),
-          ),
-          Row(
+      body:
+      TabBarView(
+        children: [
+          // #######################################
+          // ### The following part is the Tab 1 ###
+          // #######################################
+          Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton(
-                child: const Text('Generate Date',
-                    style: TextStyle(
-                        fontSize: 20.0
-                    )
+              Text(
+                //'$currentDate'.substring(0,10) //display as yyyy-mm-dd
+                '$currentDate'.substring(8,10)+'.'+'$currentDate'.substring(5,7)+'.'+'$currentDate'.substring(0,4), //display as dd.mm.yyyy
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 35.0,
                 ),
-                onPressed: () {
-                  doNotShowText();
-                  setState(() {
-                    currentDate = RandomDate.withRange(1700, 2400).random();
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.black,
-                    minimumSize: const Size(180.0,50.0)),
               ),
-              ElevatedButton(
-                child: const Text('Check Weekday',
-                    style: TextStyle(
-                        fontSize: 20.0
+              Visibility(
+                visible: _isVisible,
+                maintainSize: true,
+                maintainAnimation: true,
+                maintainState: true,
+                child: Text(
+                  //'${currentDate.weekday}',
+                    DateFormat('EEEE').format(currentDate),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 35.0,
                     )
                 ),
-                onPressed: () {showText();},
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.black,
-                    minimumSize: const Size(180.0,50.0))
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    child: const Text('Generate Date',
+                        style: TextStyle(
+                            fontSize: 20.0
+                        )
+                    ),
+                    onPressed: () {
+                      doNotShowText();
+                      setState(() {
+                        currentDate = RandomDate.withRange(1700, 2400).random();
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[850],
+                        minimumSize: const Size(180.0,50.0)),
+                  ),
+                  ElevatedButton(
+                      child: const Text('Check Weekday',
+                          style: TextStyle(
+                              fontSize: 20.0
+                          )
+                      ),
+                      onPressed: () {showText();},
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.grey[850],
+                          minimumSize: const Size(180.0,50.0))
+                  ),
+                ],
               ),
             ],
           ),
-          ],
+          // #######################################
+          // ### The following part is the Tab 2 ###
+          // #######################################
+          Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextField(
+                  controller: verifyDate,
+                  style: const TextStyle(color: Colors.white),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.all(5.0),
+                    hintText: 'Enter date as yyyy-mm-dd',
+                    hintStyle: TextStyle(
+                    color: Colors.grey),
+                  ),
+                  maxLength: 10,
+                  ),
+                ),
+                ElevatedButton(
+                  child: const Text('Check Weekday',
+                      style: TextStyle(
+                          fontSize: 20.0
+                      )
+                  ),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            //content: Text(verifyDate.text),
+                            //content: Text(DateFormat('EEEE').format(DateTime.parse('$verifyDate'.substring(6,10)+'-'+'$verifyDate'.substring(3,5)+'-'+'$verifyDate'.substring(0,2)))),
+                            //content: Text('$verifyDate'.substring(6,10)+'-'+'$verifyDate'.substring(3,5)+'-'+'$verifyDate'.substring(0,2)),
+                            content: Text(DateFormat('EEEE').format(DateTime.parse(verifyDate.text))), //TODO: fix crashes for incorrect input format
+                          );
+                        },
+                    );
+                    },
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.grey[850],
+                      minimumSize: const Size(180.0,50.0))
+              ),
+              ],
+          ),
+        ],
       ),
-    );
+    ),
+  );
   }
-}
+
+
